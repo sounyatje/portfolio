0315623333d559c7e9dc82c0a323d3e5cd793a3b
sounya94 <sounya-abattouy@live.be>
2025-11-25 00:10:01 +0100

    commit

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -13,17 +13,18 @@ export const Work = [
     id: 2,
     name: "Coach Mobile App",
     description:
-      "A mobile coaching app built with React Native for instrcutors. No demo available, use your phone and install expo to see teh results",
+      "A mobile coaching app built with React Native for instrcutors. No demo available, use your phone and install expo to see the results.",
     tags: ["react native", "expo", "nativewind"],
-
+    demo: "",
     github: "https://github.com/hellobrussels/coach-app-react-native.git",
   },
   {
     id: 3,
     name: "Customer Mobile App",
-    description: "A mobile coaching app built with React Native for customers. No demo available, use your phone and install expo to see the results",
+    description:
+      "A mobile coaching app built with React Native for customers. No demo available, use your phone and install expo to see the results.",
     tags: ["react native", "expo", "cypress"],
-
+    demo: "",
     github: "https://github.com/hellobrussels/client-app-react-native",
   },
   {
@@ -39,7 +40,7 @@ export const Work = [
     name: "Movie library",
     description: "movie library using mongo db. No demo available",
     tags: ["Node.js"],
-
+    demo: "",
     github: "https://github.com/sounyatje/movies-final",
   },
   {
@@ -48,14 +49,16 @@ export const Work = [
     description:
       "This is a content aggregator that collects information about trains, weather, and news. No demo available",
     tags: ["Node.js", "Express.js"],
+    demo: "",
     github: "https://github.com/sounyatje/agregator",
   },
   {
     id: 7,
     name: "Quizz Wordpress",
-    description: "A quizz using react as frontend and wordpress as backend. No demo available.",
+    description:
+      "A quizz using react as frontend and wordpress as backend. No demo available.",
     tags: ["typescript"],
-
+    demo: "",
     github: "https://github.com/sounyatje/quiz-wp",
   },
 ];
