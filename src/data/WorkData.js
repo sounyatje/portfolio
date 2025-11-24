38d28178e6515748a2bbe499d9c8aa29603c0f25
sounya94 <sounya-abattouy@live.be>
2025-11-25 00:06:28 +0100

    modif

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,60 +1,61 @@
 // work data
 export const Work = [
-    {
-        id: 1,
-        name: "Coach Website",
-        description: "React website with a Node.js backend to manage form submissions and data flow.",
-        tags: ["react", "node.js","react-router","jotai"],
-        demo: "https://projet-react-2-decembre.vercel.app/",
-        github: "https://github.com/sounyatje/projet-react-2-decembre"
-    },
-    {
-        id: 2,
-        name: "Coach Mobile App",
-        description: "A mobile coaching app built with React Native for instrcutors.",
-        tags: ["react native","expo","nativewind"],
-       
-        github: "https://github.com/hellobrussels/coach-app-react-native.git"
-    },
-    {
-        id: 3,
-        name: "Customer Mobile App",
-        description: "A mobile coaching app built with React Native for customers.",
-        tags: ["react native","expo","cypress"],
-       
-        github: "https://github.com/hellobrussels/client-app-react-native"
-    },
-    {
-        id: 4,
-        name: "To Do List",
-        description: "Todo list with redux applied",
-        tags: ["Redux","React"],
-        demo: "https://todolist-react-js-ashy.vercel.app/",
-        github: "https://github.com/sounyatje/todolistREACT.JS"
-    },
-    {
-        id: 5,
-        name: "Movie library",
-        description: "movie library using mongo db",
-        tags: ["Node.js"],
-      
-        github: "https://github.com/sounyatje/movies-final"
-    },
-    {
-       id: 6,
-        name: "Agregator",
-        description:"This is a content aggregator that collects information about trains, weather, and news",
-        tags: ["Node.js","Express.js"],
-    
-        github: "https://github.com/sounyatje/agregator"
+  {
+    id: 1,
+    name: "Coach Website",
+    description:
+      "React website with a Node.js backend to manage form submissions and data flow.",
+    tags: ["react", "node.js", "react-router", "jotai"],
+    demo: "https://projet-react-2-decembre.vercel.app/",
+    github: "https://github.com/sounyatje/projet-react-2-decembre",
+  },
+  {
+    id: 2,
+    name: "Coach Mobile App",
+    description:
+      "A mobile coaching app built with React Native for instrcutors. No demo available, use your phone and install expo to see teh results",
+    tags: ["react native", "expo", "nativewind"],
 
-    },
-    {
-        id: 7,
-        name: "Quizz Wordpress",
-        description: "A quizz using react as frontend and wordpress as backend.",
-        tags: ["typescript"],
+    github: "https://github.com/hellobrussels/coach-app-react-native.git",
+  },
+  {
+    id: 3,
+    name: "Customer Mobile App",
+    description: "A mobile coaching app built with React Native for customers. No demo available, use your phone and install expo to see the results",
+    tags: ["react native", "expo", "cypress"],
 
-        github: "https://github.com/sounyatje/quiz-wp"
-    }
+    github: "https://github.com/hellobrussels/client-app-react-native",
+  },
+  {
+    id: 4,
+    name: "To Do List",
+    description: "Todo list with redux applied",
+    tags: ["Redux", "React"],
+    demo: "https://todolist-react-js-ashy.vercel.app/",
+    github: "https://github.com/sounyatje/todolistREACT.JS",
+  },
+  {
+    id: 5,
+    name: "Movie library",
+    description: "movie library using mongo db. No demo available",
+    tags: ["Node.js"],
+
+    github: "https://github.com/sounyatje/movies-final",
+  },
+  {
+    id: 6,
+    name: "Agregator",
+    description:
+      "This is a content aggregator that collects information about trains, weather, and news. No demo available",
+    tags: ["Node.js", "Express.js"],
+    github: "https://github.com/sounyatje/agregator",
+  },
+  {
+    id: 7,
+    name: "Quizz Wordpress",
+    description: "A quizz using react as frontend and wordpress as backend. No demo available.",
+    tags: ["typescript"],
+
+    github: "https://github.com/sounyatje/quiz-wp",
+  },
 ];
